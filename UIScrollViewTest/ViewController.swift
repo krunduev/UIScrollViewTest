39d0c92c5c0cbbd36198507b3a1fb853fcb9d2dd
Kostyantyn Runduyev <krunduev@curiousit.eu>
2017-10-07 14:18:50 +0300

    Update

--- UIScrollViewTest/ViewController.swift
+++ UIScrollViewTest/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.width, height: 1500)
+        
+        let someView = UIView()
+        someView.frame = CGRect(x: scrollView.frame.width / 2 - 10, y: 1000, width: 20, height: 30)
+        scrollView.addSubview(someView)
+        someView.backgroundColor = UIColor.red
+        
+        let upperLevtOfVisible: CGPoint = scrollView.contentOffset
+        print("\(upperLevtOfVisible)")
+        
+        let visibleRect: CGRect = someView.convert(scrollView.bounds, from: scrollView)
+        print("\(scrollView.bounds)")
+        print("\(someView.convert(scrollView.bounds, from: scrollView))")
+
     }
 
     override func didReceiveMemoryWarning() {
